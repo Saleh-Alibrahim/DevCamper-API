52f7bb94fa5b55f1552869b91736f86df366693e
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-07 13:31:05 +0300

    fix: bug in bootcamps route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -23,9 +23,6 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router
-  .route('/:id/photo')
-  .put(bootcampPhotoUpload);
 
 router
   .route('/')
