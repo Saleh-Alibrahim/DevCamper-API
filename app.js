a2304e35446ce11f0bef211c3bd6100775446bc6
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-07 12:56:21 +0300

    server up and ruuing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
@@ -9,3 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+);
