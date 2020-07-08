7ab5888c52ed63be146eaf389fde96b59ba124e6
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-08 17:26:01 +0300

    feat: add User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ],
+        required: [true, 'Please add a Email'],
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'publisher'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a Password'],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
